4a74ec7a7eed24b65bb4430153797bb0ae32a5c7
nanaterra <anahi.guterres@escola.pr.gov.br>
2025-07-03 23:30:00 +0000

    aula-05

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
@@ -12,5 +13,18 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+
     return palavras;
 }
